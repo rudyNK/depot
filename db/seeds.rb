063336a7658ebdbee994e934b994eb1e9c1c8c1b
rudyNK <rudy@noukod.com>
2019-05-08 12:25:26 -0400

    Set-up the Making Prettier Listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create!(title: 'Shoes',
+    description:
+    %{<p>
+    <em>Best Shoes</em>
+    The Brooks® Glycerin 17 running shoe will let you go the distance with its incredible flexibility,
+    durability, and responsiveness keeping up with you every step of the way! Predecessor:
+     Glycerin 16. Support Type: Neutral. Cushioning: Maximum cushioning.
+      Surface: Road. Differential: 10 mm Please click here for more information about Glycerin 17 technology.
+       Double Jacquard Mesh upper for excellent breathability, flexibility, and stability. 
+      3D printed overlay has strategic stretch and structure for a secure fit. 
+      Plush interior bootie provides luxurious comfort without sacrificing responsiveness and long-lasting durability.
+       Centralized lace-up closure delivers a secure, adjustable fit.
+       Padded tongue and collar provides comfort against foot. 
+      Smooth fabric lining creates a great wear inside of shoe.
+       Removable cushioned insole for excellent underfoot comfort and support. 
+      IDEAL Pressure Zones help to disperse impact away from your body for a more even, effortless ride. 
+      Plush DNA LOFT Transition Zone makes sure every heel-to-toe transition is soft and smooth. 
+      Cushioned DNA LOFT midsole adapts to every step, allow for incredible underfoot support and comfort. 
+      Durable rubber outsole provides excellent traction so every step is a confident one. Imported. 
+      Product measurements were taken using size 10, width D - Medium.
+       Please note that measurements may vary by size.
+      Weight of footwear is based on a single item, not a pair.
+    Measurements: Weight: 11 oz View Zappos.com Glossary of 
+    </p>},
+    image_url: 'shoes1.jpg',
+    price: 150.00)
